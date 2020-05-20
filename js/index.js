ee8214d6a4c01a7f8ae42cf5f7fd1595f4fa5a64
yuta-0123 <yuta.fukuda.0913@gmail.com>
2020-05-20 14:20:57 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.menu').on('click', function () {
+        event.preventDefault();
+        $('#sp-nav').toggleClass('active');
+        $('.menu').toggleClass('open_active');
+    })
+    $('#sp-nav a').on('click', function () {
+        $('#sp-nav').removeClass('active');
+        $('.menu').removeClass('open_active');
+    })
+})
